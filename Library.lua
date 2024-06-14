a65c9b308932b1072a8fd897518090b2241d4ac3
rpa <yodielandia@gmail.com>
2024-06-14 02:38:47 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -32,8 +32,8 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
-    OutlineColor = Color3.fromRGB(50, 50, 50);
+    AccentColor = Color3.fromRGB(222, 92, 40);
+    OutlineColor = Color3.fromRGB(55, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
